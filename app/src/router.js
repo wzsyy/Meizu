7cce9ad0eb07a9050e278e028da6ce877cb359b6
ls240040 <ls326483836@163.com>
2019-10-08 21:00:46 +0800

    u

--- app/src/router.js
+++ app/src/router.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import footer from "./components/common/footer"
+import Cart from "./Cart"
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {path:"/footer",component:footer}
+    {path:"/footer",component:footer},
+    {path:"/Cart",component:Cart},
   ]
 })
