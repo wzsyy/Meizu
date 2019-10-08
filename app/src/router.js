73912f7a6fda7a623c70070142c1d31fdf19c143
Your Name <love0312@foxmail.com>
2019-10-08 21:06:46 +0800

    tc

--- app/src/router.js
+++ app/src/router.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import footer from "./components/common/footer"
+import header from "./components/common/TongChen/header.vue"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {path:"/footer",component:footer}
+    {path:"/footer",component:footer},
+    {path:"/header",component:header}
   ]
 })
