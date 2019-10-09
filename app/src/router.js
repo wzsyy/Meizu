686c1daa1653da76adfd1dcc8d8f0ecff2a653e4
yourname <qianyuli9371@qq.com>
2019-10-09 17:56:36 +0800

    修改footer

--- app/src/router.js
+++ app/src/router.js
@@ -2,26 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import footer from "./components/common/footer"
 import header from "./components/common/TongChen/header.vue"
-<<<<<<< HEAD
-=======
-import Cart from "./Cart"
-
->>>>>>> 73262e1166b667e83c4338400e8be1cbfb165706
 
 Vue.use(Router)
 
 export default new Router({
-<<<<<<< HEAD
   routes: [
     {path:"/footer",component:footer},
     {path:"/header",component:header}
   ]
 })
-=======
-    routes: [
-        { path: "/footer", component: footer },
-        { path: "/header", component: header },
-        { path: "/Cart", component: Cart },
-    ]
-})
->>>>>>> 73262e1166b667e83c4338400e8be1cbfb165706
